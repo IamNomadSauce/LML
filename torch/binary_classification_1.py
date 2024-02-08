3c12f80ef00d28a71b22501cf71999b5a7006be4
mgarsteck <garsteckphotography@gmail.com>
2024-02-07 22:24:55 -0500

    setup epoch training

--- torch/binary_classification_1.py
+++ torch/binary_classification_1.py
@@ -130,9 +130,41 @@ for epoch in range(epochs):
 
 
 # -----------------------------------------------------------------------
-# plt.scatter(x=X[:, 0], 
-#             y=X[:, 1], 
-#             c=y, 
-#             cmap=plt.cm.RdYlBu);
 
-# plt.show()
+# import helper_functions as helper
+
+# import requests
+# from pathlib import Path 
+# import torchvision
+# # Download helper functions from Learn PyTorch repo (if not already downloaded)
+# if Path("helper_functions.py").is_file():
+#   print("helper_functions.py already exists, skipping download")
+# else:
+#   print("Downloading helper_functions.py")
+#   request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+#   with open("helper_functions.py", "wb") as f:
+#     f.write(request.content)
+
+# from helper_functions import plot_predictions, plot_decision_boundary
+
+
+
+# Plot decision boundaries for training and test sets
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+# plot_decision_boundary(model_0, X_train, y_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+# plot_decision_boundary(model_0, X_test, y_test)
+
+
+# -----------------------------------------------------------------------
+            
+
+plt.scatter(x=X[:, 0], 
+            y=X[:, 1], 
+            c=y, 
+            cmap=plt.cm.RdYlBu);
+
+plt.show()
