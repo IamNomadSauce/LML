d918e871f40acf93f091e2a270e70b7cabb25461
mgarsteck <garsteckphotography@gmail.com>
2024-02-29 20:30:46 -0500

    Convert nn_2 to micrograd tutorial

--- GO/nn_2/main.go
+++ GO/nn_2/main.go
@@ -1,193 +1,38 @@
+// nn_2 is a go implementation of Andrej Karpathy's 'Building Micrograd'
 package main
 
 import (
-	"errors"
-	"math"
+	"fmt"
 	"math/rand"
 	"time"
-
-	"gonum.org/v1/gonum/floats"
-	"gonum.org/v1/gonum/mat"
 )
 
-type NN struct {
-	config  NN_Config
-	wHidden *mat.Dense
-	bHidden *mat.Dense
-	wOut    *mat.Dense
-	bOut    *mat.Dense
+type Neuron struct {
+	InputsN int
+	Weights []float64
+	Biases  []float64
 }
 
-type NN_Config struct {
-	inputN       int
-	outputN      int
-	hiddenN      int
-	epochs       int
-	learningRate float64
-}
+func NewNeuron(size int) *Neuron {
+	rand.Seed(time.Now().UnixNano())
 
-func New_NN(config NN_Config) *NN {
-	return &NN{config: config}
-}
+	weights := make([]float64, size)
 
-// Sigmoid Activation
-func sigmoid(x float64) float64 {
-	return 1.0 / (1.0 + math.Exp(-x))
-}
+	biases := make([]float64, size)
 
-func sigmoidPrime(x float64) float64 {
-	return sigmoid(x) * (1.0 - sigmoid(x))
-}
-
-// Train the network
-func (nn *NN) Train(X, y *mat.Dense) error {
-
-	// Initialize the weights and biases
-	randSource := rand.NewSource(time.Now().UnixNano())
-	randGen := rand.New(randSource)
-
-	wHidden := mat.NewDense(nn.config.inputN, nn.config.hiddenN, nil)
-	bHidden := mat.NewDense(1, nn.config.hiddenN, nil)
-	wOut := mat.NewDense(nn.config.hiddenN, nn.config.outputN, nil)
-	bOut := mat.NewDense(1, nn.config.outputN, nil)
-
-	wHiddenRaw := wHidden.RawMatrix().Data
-	bHiddenRaw := bHidden.RawMatrix().Data
-	wOutRaw := wOut.RawMatrix().Data
-	bOutRaw := bOut.RawMatrix().Data
-
-	for _, param := range [][]float64{
-		wHiddenRaw,
-		bHiddenRaw,
-		wOutRaw,
-		bOutRaw,
-	} {
-		for i := range param {
-			param[i] = randGen.Float64()
-		}
+	for i := range weights {
+		weights[i] = rand.Float64()*2 - 1
+		biases[i] = rand.Float64()*2 - 1
 	}
-
-	// Define the output of the neural net
-	output := new(mat.Dense)
-
-	// Use backpropagation to adjust the weights and biases
-	if err := nn.BackPropagate(X, y, wHidden, bHidden, wOut, bOut, output); err != nil {
-		return err
+	neur := &Neuron{
+		InputsN: size,
+		Weights: weights,
+		Biases:  biases,
 	}
-
-	// Define the trained neural net
-	nn.wHidden = wHidden
-	nn.bHidden = bHidden
-	nn.wOut = wOut
-	nn.bOut = bOut
-
-	return nil
+	return neur
 }
-
-// Backpropagation
-func (nn *NN) BackPropagate(X, y, wHidden, bHidden, wOut, bOut, output *mat.Dense) error {
-
-	for i := 0; i < nn.config.epochs; i++ {
-
-		// Complete feed-forward process
-		hiddenLayerInput := new(mat.Dense)
-		hiddenLayerInput.Mul(X, wHidden)
-		addBHidden := func(_, col int, v float64) float64 {
-			return v + bHidden.At(0, col)
-		}
-		hiddenLayerInput.Apply(addBHidden, hiddenLayerInput)
-
-		hiddenLayerActivations := new(mat.Dense)
-		applySigmoid := func(_, _ int, v float64) float64 {
-			return sigmoid(v)
-		}
-		hiddenLayerActivations.Apply(applySigmoid, hiddenLayerInput)
-
-		outputLayerInput := new(mat.Dense)
-		outputLayerInput.Mul(hiddenLayerActivations, wOut)
-		addBOut := func(_, col int, v float64) float64 {
-			return v + bOut.At(0, col)
-		}
-		outputLayerInput.Apply(addBOut, outputLayerInput)
-
-		// Complete the backpropagation
-		networkError := new(mat.Dense)
-		networkError.Sub(y, output)
-
-		slopeOutputLayer := new(mat.Dense)
-		applySigmoidPrime := func(_, _ int, v float64) float64 {
-			return sigmoidPrime(v)
-		}
-		slopeOutputLayer.Apply(applySigmoidPrime, output)
-		slopeHiddenLayer := new(mat.Dense)
-		slopeHiddenLayer.Apply(applySigmoidPrime, hiddenLayerActivations)
-
-		dOutput := new(mat.Dense)
-		dOutput.MulElem(networkError, slopeOutputLayer)
-		errorAtHiddenLayer := new(mat.Dense)
-		errorAtHiddenLayer.Mul(dOutput, wOut.T())
-
-		dHiddenLayer := new(mat.Dense)
-		dHiddenLayer.MulElem(errorAtHiddenLayer, slopeHiddenLayer)
-
-		// Adjust the parameters
-		wOutAdj := new(mat.Dense)
-		wOutAdj.Mul(hiddenLayerActivations.T(), dOutput)
-		wOutAdj.Scale(nn.config.learningRate, wOutAdj)
-		wOut.Add(wOut, wOutAdj)
-
-		bOutAdj, err := sumAlongAxis(0, dOutput)
-		if err != nil {
-			return err
-		}
-		bOutAdj.Scale(nn.config.learningRate, bOutAdj)
-		bOut.Add(bOut, bOutAdj)
-
-		wHiddenAdj := new(mat.Dense)
-		wHiddenAdj.Mul(X.T(), dHiddenLayer)
-		wHiddenAdj.Scale(nn.config.learningRate, wHiddenAdj)
-		wHidden.Add(wHidden, wHiddenAdj)
-
-		bHiddenAdj, err := sumAlongAxis(0, dHiddenLayer)
-		if err != nil {
-			return err
-		}
-
-		bHiddenAdj.Scale(nn.config.learningRate, bHiddenAdj)
-		bHidden.Add(bHidden, bHiddenAdj)
-	}
-	return nil
-}
-
-// Sum along axis
-func sumAlongAxis(axis int, m *mat.Dense) (*mat.Dense, error) {
-
-	numRows, numCols := m.Dims()
-
-	var output *mat.Dense
-
-	switch axis {
-	case 0:
-		data := make([]float64, numCols)
-		for i := 0; i < numCols; i++ {
-			col := mat.Col(nil, i, m)
-			data[i] = floats.Sum(col)
-		}
-		output = mat.NewDense(1, numCols, data)
-	case 1:
-		data := make([]float64, numRows)
-		for i := 0; i < numRows; i++ {
-			row := mat.Row(nil, i, m)
-			data[i] = floats.Sum(row)
-		}
-		output = mat.NewDense(numRows, 1, data)
-	default:
-		return nil, errors.New("invalid axis, must be 0 or 1")
-	}
-
-	return output, nil
-}
-
 func main() {
 
+	neuron := NewNeuron(2)
+	fmt.Println(neuron)
 }
