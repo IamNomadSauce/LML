9b7a0db091abb9164777693377a9ca4e73befda0
mgarsteck <garsteckphotography@gmail.com>
2024-02-29 21:29:07 -0500

    Setup input activation Neuron w*x+b

--- GO/nn_2/main.go
+++ GO/nn_2/main.go
@@ -11,28 +11,46 @@ type Neuron struct {
 	InputsN int
 	Weights []float64
 	Biases  []float64
+	Output  float64
 }
 
-func NewNeuron(size int) *Neuron {
+func NewNeuron(inputs []float64) *Neuron {
 	rand.Seed(time.Now().UnixNano())
 
-	weights := make([]float64, size)
+	weights := make([]float64, len(inputs))
 
-	biases := make([]float64, size)
+	biases := make([]float64, len(inputs))
 
 	for i := range weights {
-		weights[i] = rand.Float64()*2 - 1
-		biases[i] = rand.Float64()*2 - 1
+		w := weights[i]
+		b := biases[i]
+		x := inputs[i]
+
+		w = rand.Float64()*2 - 1
+		b = rand.Float64()*2 - 1
+		fmt.Println("w*x+b: ", (w*x)+b)
 	}
 	neur := &Neuron{
-		InputsN: size,
+		InputsN: len(inputs),
 		Weights: weights,
 		Biases:  biases,
 	}
 	return neur
 }
+
+type Layer struct {
+	in  int
+	out int
+}
+
 func main() {
 
-	neuron := NewNeuron(2)
+	inputs := []float64{
+		2.0,
+		4.1,
+		2.1,
+	}
+
+	neuron := NewNeuron(inputs)
 	fmt.Println(neuron)
 }
